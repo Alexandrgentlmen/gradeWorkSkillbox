0b62f9195c8e5a6a46e20bfd92c5292072668846
Alexandr Gusletsov <chegeadaun@yandex.ru>
2022-04-28 00:27:56 +0300

    42th commit

--- src/components/Redirect.jsx
+++ src/components/Redirect.jsx
@@ -13,21 +13,21 @@ export const Redirect = () => {
 		const code = url.searchParams.get('code');
 		console.log(code);
 
+		var params = new URLSearchParams();
+		params.append('client_id', ACCESS_KEY);
+		params.append('client_secret',SECRET_KEY);
+		params.append('redirect_uri', REDIRECT_URL);
+		params.append('code', code);
+		params.append('grant_type', 'authorization_code');
+
 		if (code) {
 			return axios.post('https://unsplash.com/oauth/token', {
 				headers: {
 					'Accept': 'application/json',
 					'Content-Type': 'application/x-www-form-urlencoded'
-				}
-			},
-				JSON.stringify({
-					client_id: ACCESS_KEY,
-					client_secret: SECRET_KEY,
-					redirect_uri: REDIRECT_URL,
-					code: code,
-					grant_type: 'authorization_code'
-				}),
-			).then(response => response.json()).then(data => {
+				},
+				params
+			}).then(response => response.json()).then(data => {
 				console.log(data)
 				localStorage.setItem('token', data.access_token);
 			}).catch(error => {
