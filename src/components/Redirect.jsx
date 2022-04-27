1d33cd6fad3af183ab9c5dadf264023f3eb8468e
Alexandr Gusletsov <chegeadaun@yandex.ru>
2022-04-27 23:38:44 +0300

    39th commit

--- src/components/Redirect.jsx
+++ src/components/Redirect.jsx
@@ -4,6 +4,11 @@ const ACCESS_KEY = process.env.REACT_APP_ACCESSKEY,
 	SECRET_KEY = process.env.REACT_APP_SECRETKEY,
 	REDIRECT_URL = "https://gradeskillbox.vercel.app/";
 
+	const createFormParams = (params) => {
+    return Object.keys(params)
+        .map((key) => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
+        .join('&')
+}
 
 export const Redirect = () => {
 
@@ -18,18 +23,18 @@ export const Redirect = () => {
 					'Accept': 'application/json',
 					'Content-Type': 'application/json'
 				},
-				data: {
+				data: createFormParams({
 					client_id: ACCESS_KEY,
 					client_secret: SECRET_KEY,
 					redirect_uri: REDIRECT_URL,
 					code: code,
 					grant_type: 'authorization_code'
-				}
+				}),
 			}).then(response => response.json()).then(data => {
 				console.log(data)
 				localStorage.setItem('token', data.access_token);
 			}).catch(error => {
-				alert(`Неизвестная ошибка: ${error}`);
+				alert('Неизвестная ошибка:', error.toJSON());
 			});
 		}	
 	}
