dcba76c911adff789a7d12122f671e1be63b2457
Alexandr Gusletsov <chegeadaun@yandex.ru>
2022-04-28 00:21:06 +0300

    31th commit

--- src/components/Redirect.jsx
+++ src/components/Redirect.jsx
@@ -4,6 +4,7 @@ const ACCESS_KEY = process.env.REACT_APP_ACCESSKEY,
 	SECRET_KEY = process.env.REACT_APP_SECRETKEY,
 	REDIRECT_URL = "https://gradeskillbox.vercel.app/";
 
+	
 
 export const Redirect = () => {
 
@@ -12,16 +13,21 @@ export const Redirect = () => {
 		const code = url.searchParams.get('code');
 		console.log(code);
 
-		var params = new URLSearchParams();
-		params.append('client_id', ACCESS_KEY);
-		params.append('client_secret', SECRET_KEY);
-		params.append('redirect_uri', REDIRECT_URL);
-		params.append('code', code);
-		params.append('grant_type', 'authorization_code');
-
 		if (code) {
-			return axios.post('https://unsplash.com/oauth/token',	params)
-			.then(response => response.json()).then(data => {
+			return axios.post('https://unsplash.com/oauth/token', {
+				headers: {
+					'Accept': 'application/json',
+					'Content-Type': 'application/x-www-form-urlencoded'
+				}
+			},
+				JSON.stringify({
+					client_id: ACCESS_KEY,
+					client_secret: SECRET_KEY,
+					redirect_uri: REDIRECT_URL,
+					code: code,
+					grant_type: 'authorization_code'
+				}),
+			).then(response => response.json()).then(data => {
 				console.log(data)
 				localStorage.setItem('token', data.access_token);
 			}).catch(error => {
