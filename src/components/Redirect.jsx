474b2700a5d69604e040b502aef9119047a917fa
Alexandr Gusletsov <chegeadaun@yandex.ru>
2022-04-27 23:49:14 +0300

    40th commiy

--- src/components/Redirect.jsx
+++ src/components/Redirect.jsx
@@ -4,11 +4,6 @@ const ACCESS_KEY = process.env.REACT_APP_ACCESSKEY,
 	SECRET_KEY = process.env.REACT_APP_SECRETKEY,
 	REDIRECT_URL = "https://gradeskillbox.vercel.app/";
 
-	const createFormParams = (params) => {
-    return Object.keys(params)
-        .map((key) => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
-        .join('&')
-}
 
 export const Redirect = () => {
 
@@ -17,20 +12,16 @@ export const Redirect = () => {
 		const code = url.searchParams.get('code');
 		console.log(code);
 
+		var params = new URLSearchParams();
+		params.append('client_id', ACCESS_KEY);
+		params.append('client_secret', SECRET_KEY);
+		params.append('redirect_uri', REDIRECT_URL);
+		params.append('code', code);
+		params.append('grant_type', 'authorization_code');
+
 		if (code) {
-			return axios.post('https://unsplash.com/oauth/token', {
-				headers: {
-					'Accept': 'application/json',
-					'Content-Type': 'application/json'
-				},
-				data: createFormParams({
-					client_id: ACCESS_KEY,
-					client_secret: SECRET_KEY,
-					redirect_uri: REDIRECT_URL,
-					code: code,
-					grant_type: 'authorization_code'
-				}),
-			}).then(response => response.json()).then(data => {
+			return axios.post('https://unsplash.com/oauth/token',	params)
+			.then(response => response.json()).then(data => {
 				console.log(data)
 				localStorage.setItem('token', data.access_token);
 			}).catch(error => {
