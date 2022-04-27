a27bb9186662753518c8b57c2084d0fe6d971776
Alexandr Gusletsov <chegeadaun@yandex.ru>
2022-04-27 16:27:52 +0300

    31th commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ import { User } from './User';
 
 
 const ACCESS_KEY = process.env.REACT_APP_ACCESSKEY,
-	REDIRECT_URL = "https://gradeskillbox.vercel.app";
+	REDIRECT_URL = "https://gradeskillbox.vercel.app/redirect";
 const authUrl = `https://unsplash.com/oauth/authorize?client_id=${ACCESS_KEY}&redirect_uri=${REDIRECT_URL}&response_type=code&scope=public+read_user+write_user+write_likes`;
 
 function Header() {
